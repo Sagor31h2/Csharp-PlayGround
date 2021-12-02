a4cd6f8b89bb3697ac0101d2f068fcbab1ab733e
Sagor31h2 <sagor31h2@gmail.com>
2021-12-02 14:37:01 +0600

    Action and func

---
+++ Delegate/Func/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+
+namespace Func
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Func<int, int, int> add = Sum;
+            System.Console.WriteLine(add(10, 3));
+
+            Action<string> say = SayToUser;
+            say("I m working");
+
+
+        }
+        static int Sum(int a, int b)
+        {
+            return a + b;
+        }
+
+        static void SayToUser(string msg)
+        {
+            Console.WriteLine("In from action delegate" + msg);
+        }
+    }
+}
