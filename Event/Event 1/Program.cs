f436209da5efeebe2566191bc6dc116a2912cc9c
sagor31h2 <sagor31h2@gmail.com>
2021-12-03 16:48:16 -0800

    Event Example 1

---
+++ Event/Event 1/Program.cs
@@ -0,0 +1,35 @@
+﻿using System;
+
+namespace Event
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            ProcessBusinessLogic bl = new ProcessBusinessLogic();
+            bl.ProcessCompleted += bl_ProcessCompleted; // register with an event
+            bl.StartProcess();
+        }
+        public static void bl_ProcessCompleted(object sender, EventArgs e)
+        {
+            Console.WriteLine("Process Completed!");
+        }
+    }
+    public class ProcessBusinessLogic
+    {
+        // declaring an event using built-in EventHandler
+        public event EventHandler ProcessCompleted;
+
+        public void StartProcess()
+        {
+            Console.WriteLine("Process Started!");
+            // some code here..
+            OnProcessCompleted(EventArgs.Empty); //No event data
+        }
+
+        protected virtual void OnProcessCompleted(EventArgs e)
+        {
+            ProcessCompleted?.Invoke(this, e);
+        }
+    }
+}
