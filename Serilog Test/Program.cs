23ea0247f9f7b616c39bb74c92223e9dccc4c86f
sagor31h2 <sagor31h2@gmail.com>
2021-12-14 01:13:42 +0600

    serilog tested

--- Serilog Test/Program.cs
+++ Serilog Test/Program.cs
@@ -1,2 +1,25 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿using Serilog;
+
+Log.Logger = new LoggerConfiguration()
+    .WriteTo.Console()
+    .WriteTo.Debug()
+    .WriteTo.File("logs/myapp.txt", rollingInterval: RollingInterval.Day)
+    .CreateLogger();
+
+Log.Information("Hello, world!");
+
+int a = 10, b = 0;
+try
+{
+    Log.Debug("Dividing {A} by {B}", a, b);
+
+    Console.WriteLine(a / b);
+}
+catch (Exception ex)
+{
+    Log.Error(ex, "Something went wrong");
+}
+finally
+{
+    Log.CloseAndFlush();
+}
